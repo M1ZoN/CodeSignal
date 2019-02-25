bf3e099b82bed7b5a26af3f4ceb3ff5f87ac5f44
Mukhammadnozim <31581420+M1ZoN@users.noreply.github.com>
2019-02-25 12:32:06 -0800

    added lateRide problem

--- src/mathAndLogic.py
+++ src/mathAndLogic.py
@@ -6,3 +6,21 @@
 
 def circleOfNumbers(n, firstNumber):
     return (n // 2 + firstNumber) % n
+
+# Using the bike's timer, calculate the current time. 
+# Return an answer as the sum of digits that the digital 
+# timer in the format hh:mm would show.
+# For n = 240, the output should be 4.
+# Since 240 minutes have passed, the current time is 04:00. 
+# The digits sum up to 0 + 4 + 0 + 0 = 4, which is the answer.
+
+def lateRide(n):
+    h = list(str(n // 60))
+    m = list(str(n % 60))
+    if len(h)<2:
+        h.append('0')
+    if len(m)<2:
+        m.append('0')    
+    return int(h[0])+int(h[1])+int(m[0])+int(m[1])
+
+#
