786e811e656db8414c6d35b27a298240a1f924e4
Mukhammadnozim <31581420+M1ZoN@users.noreply.github.com>
2019-02-25 12:59:07 -0800

    added phoneCall problem

--- src/mathAndLogic.py
+++ src/mathAndLogic.py
@@ -23,4 +23,24 @@ def lateRide(n):
         m.append('0')    
     return int(h[0])+int(h[1])+int(m[0])+int(m[1])
 
-#
+# Some phone usage rate may be described as follows:
+# first minute of a call costs min1 cents,
+# each minute from the 2nd up to 10th (inclusive) costs min2_10 cents
+# each minute after 10th costs min11 cents.
+# You have s cents on your account before the call. What is the duration 
+# of the longest call (in minutes rounded down to the nearest integer) you can have?
+
+def phoneCall(min1, min2_10, min11, s):
+    sum = 0
+    if s >= min1:
+        sum += 1
+        s -= min1
+    if s >= min2_10 and sum == 1:
+        while sum < 10 and s - min2_10 >= 0:
+            sum += 1
+            s -= min2_10
+    if s >= min11 and sum == 10:
+        while s - min11 >= 0:
+            sum += 1
+            s -= min11
+    return sum
